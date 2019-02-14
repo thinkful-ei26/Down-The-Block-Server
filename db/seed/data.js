792c4403c2400decd0375aff4a9e4e93ff5f7e7b
Stephen Anderson <anderssj@usc.edu>
2019-02-14 08:51:39 -0800

    small changes to seed data users and adding socket io to package.json

--- db/seed/data.js
+++ db/seed/data.js
@@ -1,31 +1,31 @@
-'use strict';
+'use strict'
 
 const comments = [
   {
     _id: '333333333333333333333301',
     date: '2018-10-26',
     content: 'Lolol, this is a funny comment',
-    userId: '000000000000000000000002',
+    userId: '000000000000000000000002'
   },
   {
     _id: '333333333333333333333302',
     date: '2017-11-26',
     content: 'This is also a funny comment!',
-    userId: '000000000000000000000002',
+    userId: '000000000000000000000002'
   },
   {
     _id: '333333333333333333333303',
     date: '2019-01-26',
     content: 'Oh No!',
-    userId: '000000000000000000000001',
+    userId: '000000000000000000000001'
   },
   {
     _id: '333333333333333333333304',
     date: '2019-01-28',
     content: 'Was anyone hurt?!',
-    userId: '000000000000000000000001',
-  },
-];
+    userId: '000000000000000000000001'
+  }
+]
 
 const posts = [
   {
@@ -35,7 +35,7 @@ const posts = [
     coordinates: 'placeholder',
     comments: ['333333333333333333333301', '333333333333333333333302'],
     content: 'Throwing a party this sat night',
-    userId: '000000000000000000000001',
+    userId: '000000000000000000000001'
   },
   {
     _id: '222222222222222222222202',
@@ -44,9 +44,9 @@ const posts = [
     coordinates: 'placeholder',
     comments: ['333333333333333333333303', '333333333333333333333304'],
     content: 'Robbery on east side of Noble. Be safe',
-    userId: '000000000000000000000002',
-  },
-];
+    userId: '000000000000000000000002'
+  }
+]
 
 const users = [
   {
@@ -59,12 +59,12 @@ const users = [
   },
   {
     _id: '000000000000000000000002',
-    firstName: 'Nate',
-    lastName: 'Pazooky',
-    username: 'npazooky',
+    firstName: 'Steve',
+    lastName: 'Anderson',
+    username: 'steve',
     // hash for "password"
     password: '$2a$10$QJCIX42iD5QMxLRgHHBJre2rH6c6nI24UysmSYtkmeFv6X8uS1kgi'
   }
-];
+]
 
-module.exports = {comments, posts, users};
+module.exports = { comments, posts, users }
