d73e81d816b4cbcf801bf6f4296a4843f37c95ad
taylormerrill <taylormerrill92@gmail.com>
2019-02-15 13:57:46 -0700

    trying to pull?

--- routes/comments.js
+++ routes/comments.js
@@ -9,36 +9,6 @@ const router = express.Router();
 
 /* ========== POST/CREATE AN ITEM ========== */
 router.post('/', (req, res, next) => {
-<<<<<<< HEAD
-  
-    const { content, postId, userId } = req.body
-    console.log('requestBody', req.body);
-    const date = '2016-10-26';
-    const newComment = { content, userId, date, postId };
-    console.log(newComment);
-    User.findById({_id:userId})
-    .then(()=> {
-      console.log(newComment);
-      console.log('1st then');
-      return Comment.create(newComment); 
-    })
-    .then(comment => {
-      console.log('2nd then');
-      console.log(comment);
-      return Post.findById({_id:postId})
-        .then(post => {
-          console.log(post);
-          post.comments.push(comment._id);
-          console.log(post);
-          return post.save();
-        });
-    })
-    .then(result => {
-      console.log(Post);
-      console.log('3rd then');
-      console.log(result);
-      return res.status(201).location(`http://${req.headers.host}/comments/${result.id}`).json(result);
-=======
   const { content, postId, userId } = req.body;
   const date = '2016-10-26';
   const newComment = { content, userId, date, postId };
@@ -55,7 +25,6 @@ router.post('/', (req, res, next) => {
     .then(post => {
       console.log('the result is', post);
       return res.status(201).location(`http://${req.headers.host}/posts/${post.id}`).json(post);
->>>>>>> 6badba3feb7a26dea7f0f475dfe6504508f38f7d
     })
     .catch(err => next(err));
     
