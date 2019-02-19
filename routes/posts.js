b7c7e640768516191a710c0d2202577356a839b0
Ethan Frigon <ethan.frigon@gmail.com>
2019-02-19 11:09:17 -0800

    config changes

--- routes/posts.js
+++ routes/posts.js
@@ -15,12 +15,13 @@ router.get('/:geo', (req, res, next) => {
   const coordsObject = JSON.parse(req.params.geo);
 
   // each 0.014631 of latitude equals one mile (this varies very slightly because the earth isn't perfectly spherical, but is close enough to true for our use case)
+  // see https://gis.stackexchange.com/questions/8650/measuring-accuracy-of-latitude-and-longitude for more info
   const latitudeMin = coordsObject.latitude - 0.014631;
   const latitudeMax = coordsObject.latitude + 0.014631;
 
-  // the longitude to mile conversion varies greatly based on the input latitude, this calculation handles that conversion
-  // ONE MILE AT MY LAT(~34) IS EQUAL TO 0.017457206881313057 degrees
-  // ONE MILE AT THE EQUATOR IS EQUAL TO 0.01445713459592308804394968917161 DEGREES
+  // the longitude to mile conversion varies greatly based on the input latitude, this calculation handles that conversion (from https://gis.stackexchange.com/questions/142326/calculating-longitude-length-in-miles)
+  // one mile at my latitude (~34)is equal to 0.017457206881313057 degrees
+  // one mile at the equator 0.01445713459592308804394968917161 degrees
   const oneDegreeLongitude = Math.cos(coordsObject.latitude * Math.PI/180) * 69.172;
   const oneMileLongitudeInDegrees = 1/oneDegreeLongitude;
   const longitudeMin = coordsObject.longitude - oneMileLongitudeInDegrees;
