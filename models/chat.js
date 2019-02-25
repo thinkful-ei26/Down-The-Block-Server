c7cd6393c145651e0ee13d7b7278151d178d745b
taylormerrill <taylormerrill92@gmail.com>
2019-02-25 11:16:09 -0700

    implementing chat feature- still in progress

---
+++ models/chat.js
@@ -0,0 +1,25 @@
+// 'use strict';
+
+// const mongoose = require('mongoose');
+
+// const chatSchema = new mongoose.Schema({
+//   date: { type: String, required: true },
+//   content: { type: String, required: true },
+//   image: { type: String },
+//   messagess: [{type: mongoose.Schema.Types.ObjectId, ref: 'Message'}],
+//   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+// //   audience: { type: String, required: true }
+// });
+
+// //// Customize output for `res.json(data)`, `console.log(data)` etc.
+// chatSchema.set('toJSON', {
+//   virtuals: true,     // include built-in virtual `id`
+//   transform: (doc, ret) => {
+//     delete ret._id; // delete `_id`
+//     delete ret.__v; //delete _v
+//   }
+// });
+
+// const Chat = mongoose.model('Chat', ChatSchema); 
+
+// module.exports = Chat;
